63c4191e61ab9f41883d7fbbd2a98ce388dd9334
Zen <amaar5ali@gmail.com>
2024-02-01 14:48:10 -0500

    finished, time to deploy

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ZenTheMC</div>
+          <div className="right">
+            <p>Queens, NY, U.S.</p>
+            <p>Open: 24/7</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Amaar Ali</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By ZenTheMC.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
