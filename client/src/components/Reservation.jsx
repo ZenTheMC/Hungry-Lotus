a4701c32bbdac7b71bf0c690462c18554fff71cf
Zen <amaar5ali@gmail.com>
2024-02-01 17:40:55 -0500

    updated reservation to relative path instead of localhost

--- client/src/components/Reservation.jsx
+++ client/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
@@ -28,6 +28,7 @@ const Reservation = () => {
         }
       );
       toast.success(data.message);
+      // Reset form fields and navigate on success
       setFirstName("");
       setLastName("");
       setPhone(0);
